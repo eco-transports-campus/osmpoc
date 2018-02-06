ac135d33b7643d56f5f1d54ae624f673c5e2d892
Thomas Le Sciellour <thomas.lesciellour@gmail.com>
2018-02-06 11:17:27 +0100

    Initialize POC

---
+++ builder.js
@@ -0,0 +1,37 @@
+/**
+ *  Config Variable
+**/
+const Config = {
+    JS_IN: 'app/src',
+    JS_OUT: 'app/www/js'
+  };
+
+
+
+/**
+ *  Dependencies
+**/
+const fs = require('fs'),
+      browserify = require('browserify');
+
+
+
+/**
+ *  Main Process
+**/
+fs
+  // Retrieve all JS source file
+  .readdirSync(Config.JS_IN)
+  .forEach(function(file) {
+    let inPath = Config.JS_IN + '/' + file,
+        outPath = Config.JS_OUT + '/' + file;
+
+    console.log('\tBuild: ' + inPath + ' -> ' + outPath);
+
+    // For each JS src write the output into the application
+    browserify(inPath)
+      .transform("babelify", { presets: ["env"] })
+      .bundle()
+      .pipe(fs.createWriteStream(outPath));
+
+  });
